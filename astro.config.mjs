76136f99b107f35ea699c14e4f1339308f9e1668
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2026-03-31 14:12:04 +0100

    feat: Integrate React, Markdoc, and Keystatic into Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify({
     middlewareMode: 'edge'
-  })
+  }),
+
+  integrations: [react(), markdoc(), keystatic()]
 });
